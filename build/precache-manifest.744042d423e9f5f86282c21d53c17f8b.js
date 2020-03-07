8ba5794067a2211f76a4c6c61c90527ba022223a
Cloud User <ec2-user@ip-10-0-1-102.ec2.internal>
2020-03-07 23:17:02 +0000

    first draft

---
+++ build/precache-manifest.744042d423e9f5f86282c21d53c17f8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "39cc881b550854b2d17d",
+    "url": "/static/css/main.67ade174.chunk.css"
+  },
+  {
+    "revision": "39cc881b550854b2d17d",
+    "url": "/static/js/main.39cc881b.chunk.js"
+  },
+  {
+    "revision": "dd34be83fed3083041d6",
+    "url": "/static/js/1.dd34be83.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7940e6ce3d1c814582d82554510dff6b",
+    "url": "/static/media/logo.7940e6ce.png"
+  },
+  {
+    "revision": "1e1aa8757f1b7de097af3448540a8786",
+    "url": "/index.html"
+  }
+];
